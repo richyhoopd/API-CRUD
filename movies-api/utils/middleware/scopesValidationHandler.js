e00f0cb5d8d10d9bb5d297bf719bbee08357bf79
Ricardo Martinez <theilluminatiduck@gmail.com>
2020-05-07 13:34:13 -0500

    primer commit, enviando a repo en la nube

---
+++ movies-api/utils/middleware/scopesValidationHandler.js
@@ -0,0 +1,21 @@
+const boom = require('@hapi/boom');
+
+function scopesValidationHandler(allowedScopes){
+    return function (req, res, next) {
+        if (!req.user || (req.user && !req.user.scopes)) {
+            next(boom.unauthorized('Missing Scopes'));
+        }
+
+        const hasAcces = allowedScopes
+        .map(allowedScopes => req.user.scopes.includes(allowedScopes))
+        .find(allowed => Boolean(allowed));
+
+        if (hasAcces) {
+            next();
+        } else {
+            next(boom.unauthorized('insuficient scopes'))
+        }
+    };
+}
+
+module.exports = scopesValidationHandler;
